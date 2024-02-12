f5edd73b1e6b799b8cd409f944ca5f40b8f8a6d1
Kunj-Patel-123 <153956382+Kunj-Patel-761@users.noreply.github.com>
2024-02-12 16:15:01 -0500

    Assignment1_Updated

---
+++ K_Patel_MyCar/src/app/helper-files/content-interface.ts
@@ -0,0 +1,9 @@
+export interface Content{
+    id: number;
+    title: string;
+    description: string;
+    manufacturer: string;
+    imgURL?: string;
+    type?: string;
+    tags?: string[];  
+}
